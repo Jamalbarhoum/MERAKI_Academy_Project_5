8bfeb152c503b5c827837c837f2165633611f226
Jamalbarhoum <jamallbarhoum@gmail.com>
2024-02-19 14:18:18 +0300

    sss

--- frontend/src/components/Profile/Profile.js
+++ frontend/src/components/Profile/Profile.js
@@ -1,18 +1,15 @@
-/* eslint-disable eqeqeq */
 /* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable no-unused-vars */
-import React, { useEffect, useState ,useRef} from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useLocation, useNavigate } from "react-router-dom";
 import Loader from "../Loader/Loader";
 import "../Profile/Profile.css";
 import { CiBookmark } from "react-icons/ci";
 import { BsPostcard } from "react-icons/bs";
-import { useDispatch, useSelector } from "react-redux";
-import Button from "react-bootstrap/Button";
-import Modal from "react-bootstrap/Modal";
+import { UseDispatch, useDispatch, useSelector } from "react-redux";
 const Profile = () => {
-  const  arr  = useRef([]);
+  const dispatch = useDispatch();
 
   const { token } = useSelector((state) => {
     return {
@@ -22,21 +19,16 @@ const Profile = () => {
   const location = useLocation();
   const queryParams = new URLSearchParams(location.search);
   const searchQuery = queryParams.get("prf") || "";
-
+  console.log(searchQuery);
   const [profileInfo, setProfileInfo] = useState();
   const [loader, setLoader] = useState(true);
-  const [showPost, setShowPost] = useState(false);
   const [countFollowers, setCountFollowers] = useState(0);
   const [countFollowing, setCountFollowing] = useState(0);
   const [myPosts, setMyPosts] = useState();
-  const [myPostsLength, setMyPostsLength] = useState(0);
-  const [modalShow, setModalShow] = React.useState(false);
-  const [followers, setFollowers] = useState();
-  const [following, setFollowing] = useState();
-  console.log(following);
+  console.log(profileInfo);
   window.scrollTo(0, 0);
-
   useEffect(() => {
+    
     axios
       .get(`http://localhost:5000/users/${searchQuery}`)
       .then((result) => {
@@ -51,21 +43,9 @@ const Profile = () => {
     axios
       .get(`http://localhost:5000/followers/Followers/${searchQuery}`)
       .then((result) => {
+        console.log(result.data);
         setCountFollowers(result.data.length);
         setLoader(false);
-        setFollowers(result?.data?.result);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }, []);
-  useEffect(() => {
-    axios
-      .get(`http://localhost:5000/post/mypost/${searchQuery}`)
-      .then((result) => {
-        console.log("raed", result.data.result.length);
-        setMyPosts(result?.data?.result);
-        setMyPostsLength(result?.data?.result?.length);
       })
       .catch((err) => {
         console.log(err);
@@ -75,74 +55,14 @@ const Profile = () => {
     axios
       .get(`http://localhost:5000/followers/Following/${searchQuery}`)
       .then((result) => {
+        console.log(result.data);
         setCountFollowing(result.data.length);
         setLoader(false);
-        setFollowing(result?.data?.result);
-        console.log(result.data.result);
-
-        result.data.result.forEach((element) => {
-          console.log(element.id);
-           arr.current.push(element.id)
-          
-        });
       })
       .catch((err) => {
         console.log(err);
       });
   }, []);
-
-  function MyVerticallyCenteredModal(props) {
-    return (
-      <Modal
-        {...props}
-        size="lg"
-        aria-labelledby="contained-modal-title-vcenter"
-        centered
-      >
-        <Modal.Header closeButton style={{ borderBottom: "0px" }}>
-          <Modal.Title id="contained-modal-title-vcenter">
-            <h3>Followers</h3>
-          </Modal.Title>
-        </Modal.Header>
-        <Modal.Body id="Modal.Body">
-          {followers?.map((item, i) => {
-            console.log(item);
-            return (
-              <div key={i} className="pop-main">
-                <div style={{ display: "flex" }}>
-                  <img
-                    className="pop-img"
-                    alt=""
-                    src={item.profile_picture_url}
-                    style={{
-                      width: "80px",
-                      height: "80px",
-                      borderRadius: "50%",
-                    }}
-                  />
-                  <div className="name-bio-pop">
-                    <p className="name-pop">{item.username}</p>
-                    <p className="bio-pop">{item.bio}</p>
-                  </div>
-                </div>
-                {/* <div> */}
-                {arr.current?.includes(item.id) ? (
-                  <button className="button-pop">Follow</button>
-                ) : (
-                  <button className="button-pop">UnFollow</button>
-                )}
-                {/* </div> */}
-              </div>
-            );
-          })}
-        </Modal.Body>
-        <Modal.Footer style={{ borderBottom: "0px" }}>
-          <Button onClick={props.onHide}>Close</Button>
-        </Modal.Footer>
-      </Modal>
-    );
-  }
-console.log(arr.current);
   return (
     <div id="mainPage">
       {loader ? (
@@ -170,52 +90,51 @@ console.log(arr.current);
                 </div>
                 <div className="followerr">
                   <p className="bio">{elm.bio}</p>
-                  <p className="followers" onClick={() => setModalShow(true)}>
-                    {countFollowers} followers
-                  </p>
-                  <p className="followers" onClick={() => setModalShow(true)}>
-                    {countFollowing} following
-                  </p>
+                  <p className="followers">{countFollowers} followers</p>
+                  <p className="followers">{countFollowing} following</p>
                 </div>
               </div>
-              <MyVerticallyCenteredModal
-                show={modalShow}
-                onHide={() => setModalShow(false)}
-              />
             </div>
           );
         })
       )}
-      <div className="wrapping">
-        <div className="level2">
-          <div className="p-s">
-            <BsPostcard />
-            <p
-              className="posts-saved"
-              onClick={() => {
-                setShowPost(!showPost);
-              }}
-            >
-              posts ({myPostsLength})
-            </p>
-          </div>
-          <div className="p-s">
-            <CiBookmark />
-            <p className="posts-saved">saves</p>
-          </div>
-        </div>
-        <div className={showPost ? "grid-media" : "none"}>
-          {myPosts?.map((ele, i) => {
-            return (
-              <img
-                key={ele.id}
-                className="img-post"
-                alt=""
-                src={ele.media_url}
-              ></img>
+      <div className="level2">
+        <div className="p-s">
+          <BsPostcard />
+          <p
+            className="posts-saved"
+            onClick={async () => {
+          try {
+            const result = await axios.get(
+              `http://localhost:5000/post/mypost/${searchQuery}`,
+              {
+                headers: {
+                  Authorization: `Bearer ${token}`,
+                },
+              }
             );
-          })}
+            setMyPosts(result.data.result)
+            console.log(result.data.result);
+          } catch (error) {
+            console.log("from post by user id",error);
+          }
+            }}
+          >
+            posts
+          </p>
         </div>
+        <div className="p-s">
+          <CiBookmark />
+          <p className="posts-saved">saves</p>
+        </div>
+      </div>
+      <div className="grid-media">
+        {myPosts?.map((ele,i)=>{
+          console.log(ele);
+          return (
+            <img className="img-post" alt="" src={ele.media_url}></img>
+          )
+        })}
       </div>
     </div>
   );
